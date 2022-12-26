9b58509f14d429f6ed5c5720acbaff3c941866d9
Vikash Tiwari <95138490+vikashtiwari97@users.noreply.github.com>
2022-12-26 11:35:11 +0530

    Add files via upload

---
+++ Area.cpp
@@ -0,0 +1,12 @@
+//Write a C++ program to take length and breadth of a rectangle and print its area.
+#include<iostream>
+
+using namespace std;
+int main(){
+    int lenght=7,breadth=4;
+    cout<<"The value of lenght is 7"<<endl;
+    cout<<"The value of breadth is 4"<<endl;
+    int area=lenght*breadth;
+    cout<<"Area of rectangle is "<<area;
+    return 0;
+}
