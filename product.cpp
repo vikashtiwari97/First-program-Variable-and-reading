fe6b99e9660da283ec133139be7969ff3720c191
Vikash Tiwari <95138490+vikashtiwari97@users.noreply.github.com>
2022-12-26 11:25:05 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,12 @@
+// Take two integer values in two variables X and Y and find their product
+
+#include<iostream>
+
+using namespace std;
+int main(){
+    int x=2,y=4;
+    cout<<"The value of x is 2"<<endl;
+    cout<<"The value of y is 4"<<endl;
+    cout<<"Product of x and y is "<<x*y;
+    return 0;
+}
