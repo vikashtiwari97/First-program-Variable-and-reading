355252cc74c3df9702dfc84470db9c2bbff84794
Vikash Tiwari <95138490+vikashtiwari97@users.noreply.github.com>
2022-12-26 11:37:59 +0530

    Add files via upload

---
+++ cube.cpp
@@ -0,0 +1,11 @@
+//Write a C++ program to calculate the cube of a number.
+
+#include<iostream>
+
+using namespace std;
+int main(){
+    int a=4;
+    cout<<"The value of a is 4"<<endl;
+    cout<<"Cube of a is "<<a*a*a;
+    return 0;
+}
